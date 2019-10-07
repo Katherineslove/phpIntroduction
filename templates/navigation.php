a95c48bceb6677383f22225b93ffaa14ae58e7ac
Katherineslove <48259743+Katherineslove@users.noreply.github.com>
2019-10-08 11:42:04 +1300

    included header, navigation and footer

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,5 +1,5 @@
 <ul>
-    <li><a href="index.php">Home</a></li>
+    <li><a href="index.php" class="active">Home</a></li>
     <li><a href="contact.php">Contact</a></li>
     <li><a href="about.php">About</a></li>
     <li><a href="delieveries.php">Delieveries</a></li>
