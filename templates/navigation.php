7ba98003955da10ce4437871b2f5dc3ef29c3c0e
Katherineslove <48259743+Katherineslove@users.noreply.github.com>
2019-10-08 10:03:48 +1300

    edited navigation

--- templates/navigation.php
+++ templates/navigation.php
@@ -3,6 +3,5 @@
     <li><a href="contact.php">Contact</a></li>
     <li><a href="about.php">About</a></li>
     <li><a href="delieveries.php">Delieveries</a></li>
-    <li><a href="navigation.php">Navigation</a></li>
     <li><a>Shop</a></li>
 </ul>
