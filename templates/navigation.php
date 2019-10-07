eb77687375cab3ff541be81c8f68bca9719f8fb2
Katherineslove <48259743+Katherineslove@users.noreply.github.com>
2019-10-08 11:52:41 +1300

    adding class active

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,7 +1,7 @@
 <ul>
-    <li><a href="index.php" class="active">Home</a></li>
-    <li><a href="contact.php">Contact</a></li>
-    <li><a href="about.php">About</a></li>
-    <li><a href="delieveries.php">Delieveries</a></li>
+    <li><a href="index.php" class="<?php if($title === 'Home'){echo "active";} ?>">Home</a></li>
+    <li><a href="contact.php" class="<?php if($title === 'Contact'){echo "active";} ?>">Contact</a></li>
+    <li><a href="about.php" class="<?php if($title === 'About'){echo "active";} ?>">About</a></li>
+    <li><a href="delieveries.php" class="<?php if($title === 'Delivery'){echo "active";} ?>">Delieveries</a></li>
     <li><a>Shop</a></li>
 </ul>
